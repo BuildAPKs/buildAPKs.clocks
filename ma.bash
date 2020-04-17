bc49374b45081a033da6b0a8a6f8a7de70f20d58
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-17 03:33:15 -0400

    commit 440380931 on 20200417

--- ma.bash
+++ ma.bash
@@ -9,5 +9,6 @@ shopt -s nullglob globstar
 cd "$JDR"
 # _AT_ username/repository commit
 _AT_ AvatarQing/MiClockView 39143bef4a008de385e8f68ae823a3b099192e30
+_AT_ lithium/alarming 85cf7df9f03672afc65a668fdd1e40551ae33082
 _AT_ rahulaga/WorldClock bb6a003fe0ef563e8206bb8ed7c88d1d299eb509
 # ma.bash OEF 
