572564be68b4a1c34a900026f6f8c0ea2d5c4dc8
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-08 10:03:28 -0400

    commit 205528506 on 20210408

--- ma.bash
+++ ma.bash
@@ -9,6 +9,10 @@ shopt -s nullglob globstar
 cd "$JDR"
 # _AT_ username/repository commit
 _AT_ AvatarQing/MiClockView 39143bef4a008de385e8f68ae823a3b099192e30
+_AT_ Born2Crawl/LCD-Clock 8c46e7b4325df60b83cafe69290a554148ac9257
+_AT_ gregkh/android-clock 1b053fe3d264ecf9b26879258675b83d9161a58c
 _AT_ lithium/alarming 85cf7df9f03672afc65a668fdd1e40551ae33082
 _AT_ rahulaga/WorldClock bb6a003fe0ef563e8206bb8ed7c88d1d299eb509
+_AT_ singpolyma/OrdClock b2a056993c3b1f67df2d19cfbf11e84a5e8b417a
+_AT_ vermasque/song-alarm 5191267f53eaddf4d41742f169b55da0d86cc36a
 # ma.bash OEF 
