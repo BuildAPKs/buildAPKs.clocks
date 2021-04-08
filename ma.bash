ecddb7a8fcd6503576bdb5d7e05c9a67b81033c3
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-08 10:18:02 -0400

    commit 165887558 on 20210408

--- ma.bash
+++ ma.bash
@@ -10,9 +10,12 @@ cd "$JDR"
 # _AT_ username/repository commit
 _AT_ AvatarQing/MiClockView 39143bef4a008de385e8f68ae823a3b099192e30
 _AT_ Born2Crawl/LCD-Clock 8c46e7b4325df60b83cafe69290a554148ac9257
+_AT_ arighi/PicoClock ffa1645910fe1c368e747ff777d15d35af8c1ede
 _AT_ gregkh/android-clock 1b053fe3d264ecf9b26879258675b83d9161a58c
 _AT_ lithium/alarming 85cf7df9f03672afc65a668fdd1e40551ae33082
+_AT_ nlantz/BrewClock a836a555e73c3f6e9e0978a5e3c04d19facb92e6
 _AT_ rahulaga/WorldClock bb6a003fe0ef563e8206bb8ed7c88d1d299eb509
+_AT_ shakalaca/BeautyClockLiveWallpaper 8abb9d220c780f5a9d4434de716f71b36714720a
 _AT_ singpolyma/OrdClock b2a056993c3b1f67df2d19cfbf11e84a5e8b417a
 _AT_ vermasque/song-alarm 5191267f53eaddf4d41742f169b55da0d86cc36a
 # ma.bash OEF 
